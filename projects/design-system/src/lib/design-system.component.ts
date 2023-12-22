36a16568ede811c7fc1e8a15a9fb2016cf60cb20
Royer Adames <royeraadames@gmail.com>
2023-12-21 22:59:12 -0400

    feature: angular library init

---
+++ projects/design-system/src/lib/design-system.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-prefix-design-system',
+  template: `
+    <p>
+      design-system works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class DesignSystemComponent {
+
+}
